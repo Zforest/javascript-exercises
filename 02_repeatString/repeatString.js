e2768d45f7eb01f5ed7a5a6e77b7d2c3c05581ea
Zforest <30865263+Zforest@users.noreply.github.com>
2023-06-13 14:56:20 -0400

    Saving Practice Exercises
    Completed exercise 1
    
    Exercise two is nearly complete.
     -Need to turn reversed array into single string without seperations

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+if (num < 0) return 'ERROR';
+let greet = '';
+for (let i = 0; i < num; i++) {
+    greet += string;
+}
+return greet;
 };
-
+repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    word = string.split('');
+    wordLength = word.length;
+    reverseMe = word.slice(0, wordLength);
+    reversedArray = reverseMe.reverse();
+    reversedString = reversedArray.toString();
+    return reversedString;
+    };
+    reverseString('Hello');
 
 // Do not edit below this line
 module.exports = reverseString;
