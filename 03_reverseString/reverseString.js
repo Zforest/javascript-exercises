c5e64f0d3a32da7e6e5f7d72f59291ef41c8a72d
Zforest <30865263+Zforest@users.noreply.github.com>
2023-06-15 14:47:26 -0400

    04_removeFromArray in Progress
    Using forEach loop to cycle through elements
    Using Splice and indexof to find and remove match to 2nd parameter

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,12 +1,7 @@
 const reverseString = function(string) {
-    word = string.split('');
-    wordLength = word.length;
-    reverseMe = word.slice(0, wordLength);
-    reversedArray = reverseMe.reverse();
-    reversedString = reversedArray.toString();
-    return reversedString;
+    return string.split('').reverse().join('');
     };
-    reverseString('Hello');
+    reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+/*
+Create a function that takes an array as the first argument.
+Then the function removes from the array whatever is entered as the second argument.
 
-};
+determine if element in array matches second argument
+    if yes remove element
+        find element position (for each loop)
+        note position (length)
+        remove position (splice)
+        return array
+    if no return no matching results
+*/
 
+const removeFromArray = function(array, removeNum) {
+    array.forEach(num => array.splice(array.indexOf(removeNum))); 
+    return array;
+        }
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
